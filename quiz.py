788e3bf4c329d57834c903ee112caf8e2403a676
Cool-Coder-max <ssriadvaith@gmail.com>
2024-07-26 16:43:10 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,62 @@
+print("Welcome to my SOCCER / FOOTBALL QUIZ")
+
+playing = input("Do you want play ?")
+
+score = 0
+
+if playing == "yes":
+    quit()
+
+print("Lets start the game :)")
+
+answer = str(input("Who is The GOAT of SOCCER/FOOTBALL ? "))
+
+if answer == str("Cristiano Ronaldo" ):
+    print("The answer is correct you are on to the next round")
+else:
+    print("Wrong Answer")
+    quit()
+
+answer = str(input("name on belgium footbalLer {in their shortform only} ? "))
+
+if answer == str("KDB"):
+    print("The answer is correct you are on to the next round")
+else:
+    print("Wrong Answer")
+    quit()
+
+answer = str(input("Who won the 2018 world cup  ? "))
+
+if answer == str("France"):
+    print("The answer is correct you are on to the next round")
+else:
+    print("Wrong Answer")
+    quit()
+
+answer = str(input("which country won the most worldcup ? "))
+
+if answer == str("Brazil"):
+    print("The answer is correct you are on to the next round")
+else:
+    print("Wrong Answer")
+    quit()
+
+
+answer = str(input("name a icon form brazil his shirt number is 9 and 99 ? "))
+
+if answer == str("R9"):
+     print("The answer is correct you are on to the next round")
+else:
+     print("Wrong Answer")
+     quit()
+
+answer = str(input("which country won won the 2023 world cup ? "))
+
+if answer == str("Argentina"):
+    print("The answer is correct and the quiz is completed")
+else:
+    print("Wrong Answer")
+    quit()
+
+print("Congratulations")
+print()
